3f798d25c399d817a785e3f68ec4da7ac6cf6a90
Mharie-Lhou <marielou.j.allain@gmail.com>
2026-04-15 20:57:15 +0200

    refacto : improve mocking

--- src/hooks/useJob.ts
+++ src/hooks/useJob.ts
@@ -1,30 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { api, USE_MOCK } from "../lib/axios";
 import type { JobDetail } from "@/types/job";
-
-const MOCK_JOB: JobDetail = {
-  id: "mock-job-1",
-  status: "printing",
-  total: 2,
-  printed: 1,
-  ticketNumber: 1234,
-  retryCount: 0,
-  createdAt: "2025-01-01T00:00:00Z",
-  updatedAt: "2025-01-01T00:00:00Z",
-  photos: [
-    {
-      photoStorageKey:
-        "https://live.staticflickr.com/65535/55204653200_b136eb8c2f_c.jpg",
-      copies: 1,
-    },
-  ],
-};
+import { mockFetchedJobDetail } from "@/mocks/mockFetchedJobs";
 
 export function useJob(jobId: string) {
   return useQuery({
     queryKey: ["job", jobId],
     queryFn: async (): Promise<JobDetail> => {
-      if (USE_MOCK) return MOCK_JOB;
+      if (USE_MOCK) return mockFetchedJobDetail(jobId);
+
       const { data } = await api.get<JobDetail>(`/jobs/${jobId}`);
 
       if (!data?.photos) {
--- src/hooks/useJobs.ts
+++ src/hooks/useJobs.ts
@@ -1,75 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
-import { api } from "../lib/axios";
+import { api, USE_MOCK } from "../lib/axios";
 import type { JobsResponse } from "@/types/job";
+import { mockFetchedJobs } from "@/mocks/mockFetchedJobs";
 
 export function useJobs(page: number, limit: number) {
   const offset = page * limit;
   return useQuery({
     queryKey: ["jobs", page, limit],
     queryFn: async () => {
-      // const { data } = await api.get<JobsResponse>("/jobs", {
-      //   params: { sort: "status", limit, offset },
-      // });
+      if (USE_MOCK) {
+        return mockFetchedJobs();
+      }
+      const { data } = await api.get<JobsResponse>("/jobs", {
+        params: { sort: "status", limit, offset },
+      });
 
-      // return data;
-
-      const re: JobsResponse = {
-        jobs: [
-          {
-            id: "ss",
-            status: "printing",
-            total: 2,
-            printed: 1,
-            retryCount: 0,
-            createdAt: "2025",
-            updatedAt: "2025",
-            ticketNumber: 2,
-          },
-          {
-            id: "ssszdfze",
-            status: "printing",
-            total: 2,
-            printed: 1,
-            retryCount: 0,
-            createdAt: "2025",
-            updatedAt: "2025",
-            ticketNumber: 2,
-          },
-          {
-            id: "ssfzevzg",
-            status: "requeued",
-            total: 2,
-            printed: 1,
-            retryCount: 0,
-            createdAt: "2025",
-            updatedAt: "2025",
-            ticketNumber: 2,
-          },
-          {
-            id: "sezfzs",
-            status: "error",
-            total: 2,
-            printed: 1,
-            retryCount: 0,
-            createdAt: "2025",
-            updatedAt: "2025",
-            ticketNumber: 2,
-          },
-          {
-            id: "sfzgzefrvs",
-            status: "done",
-            total: 2,
-            printed: 1,
-            retryCount: 0,
-            createdAt: "2025",
-            updatedAt: "2025",
-            ticketNumber: 2,
-          },
-        ],
-        total: 1,
-      };
-
-      return re;
+      return data;
     },
     refetchInterval: 10000,
   });
